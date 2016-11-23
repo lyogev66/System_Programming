72da0c0ff388c6e843d34b341fb4059d25a2780b
yogev laks <lyogev66@users.noreply.github.com>
2016-11-23 20:59:59 +0200

    some updated - command line arguments
    now working with commandline arguments but i have a problem with the memory free at the end

--- ex2/TestFile/test_file_main.c
+++ ex2/TestFile/test_file_main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>//for FILE object
-
+#include <string.h> //strlen
 #include <windows.h> //for thread and Sleep
+
+#define OK 0
+#define ERROR 1
+
 
 void test_file_extension(char *inputfile_name,FILE *output_file)
 {
@@ -55,26 +59,63 @@ void test_file_contant(FILE *input_file,FILE *output_file)
 	fprintf(output_file,"\n");
 }
 
+void output_file_name();
 
-void main()//int argc ,char * argv[])
+int main(int argc ,char * argv[])
 {
-	//char *name_of_tested_file;
-	//char *output_file_path;
+	char *name_of_tested_file;
+	char *output_file_path;
+	char *temp_name_of_tested_file;
 	//	name_of_tested_file=argv[1];
 	//	output_file_path=argv[2];
-	char inputfile_name[]="test1.txt";
+	//char inputfile_name[]="test1.txt";
 	//char outputfilepath[]="C:\Users\yogev\Documents\Visual Studio 2010\Projects\system_programming\ex2\TestFile";
-	char outputfile_name[]="test1_log.txt";
+	//char outputfile_name[]="test1_log.txt";
 	FILE *input_file;
 	FILE *output_file;
+	size_t input_string_len,output_file_name_size;
+
+	if (argc<3)
+		return ERROR;
 
 
-	input_file=fopen(inputfile_name,"r");
-	output_file=fopen(outputfile_name,"w");
+	//input string
+	input_string_len=strlen(argv[1]);
+	name_of_tested_file=(char*) malloc(sizeof(char)*input_string_len);
+	if (name_of_tested_file==NULL)
+		return ERROR;
 
 
+	name_of_tested_file=strcpy(name_of_tested_file,argv[1]);
+	temp_name_of_tested_file=(char*) malloc(sizeof(char)*input_string_len);
+	if (temp_name_of_tested_file==NULL)
+		return ERROR;
 
-	test_file_extension(inputfile_name,output_file);
+	temp_name_of_tested_file=strcpy(temp_name_of_tested_file,argv[1]);
+	temp_name_of_tested_file=strtok(temp_name_of_tested_file,".");
+	temp_name_of_tested_file=strcat(temp_name_of_tested_file,"_log.txt");
+	//printf("inname=%s\n",temp_name_of_tested_file);
+
+	//need to handle the file extantion length
+	output_file_name_size=input_string_len+strlen(argv[2])+5; //<outputfile directory> + \+inputfile name with extinsion +_log
+	output_file_path=(char*) malloc(sizeof(char)*output_file_name_size);
+	if (output_file_path==NULL)
+		return ERROR;
+	output_file_path=strcpy(output_file_path,argv[2]);
+	output_file_path=strcat(output_file_path,temp_name_of_tested_file);
+	//printf("outname=%s\n",output_file_path);
+	
+
+
+	input_file=fopen(name_of_tested_file,"r");
+	if (input_file==NULL)
+		return ERROR;
+	output_file=fopen(output_file_path,"w");
+	if (output_file==NULL)
+		return ERROR;
+
+
+	test_file_extension(name_of_tested_file,output_file);
 	test_file_size(input_file,output_file);
 	test_file_timing(input_file,output_file);
 	test_file_contant(input_file,output_file);
@@ -83,7 +124,15 @@ void main()//int argc ,char * argv[])
 	fclose(output_file);
 	fclose(input_file);
 
+	//problem with free
+	//free(output_file_path);
+	//free(name_of_tested_file);
+	//free(temp_name_of_tested_file);
 
+
+
+
+	return OK;
 
 
 
